1b7bce364aa9fba720b011e26fa46b00ed1a8efd
Cod <yeetf7927@gmail.com>
2024-09-10 09:38:32 +0200

    Add python lsp

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -7,3 +7,5 @@ require("lspconfig").lua_ls.setup {}
 require("lspconfig").zls.setup {}
 require("lspconfig").hyprls.setup {}
 require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").pylsp.setup {}
+require("lspconfig").pylyzer.setup {}
